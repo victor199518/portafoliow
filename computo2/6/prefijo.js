c9212d85faf050ae0f08a6cc6c33c0cad339eaec
victor199518 <gmachuca2016@gmail.com>
2018-05-20 01:33:20 -0600

    actualizando

---
+++ computo2/6/prefijo.js
@@ -0,0 +1,17 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+ <head>
+  <title>Index of /1/USANDO FLEXBOX/js</title>
+ </head>
+ <body>
+<h1>Index of /1/USANDO FLEXBOX/js</h1>
+  <table>
+   <tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>
+   <tr><th colspan="5"><hr></th></tr>
+<tr><td valign="top"><img src="/icons/back.gif" alt="[PARENTDIR]"></td><td><a href="/1/USANDO%20FLEXBOX/">Parent Directory</a>       </td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="preefix.js">preefix.js</a>             </td><td align="right">2018-04-05 08:26  </td><td align="right">6.6K</td><td>&nbsp;</td></tr>
+   <tr><th colspan="5"><hr></th></tr>
+</table>
+<address>Apache/2.4.26 (Win32) OpenSSL/1.0.2l PHP/7.1.7 Server at 192.168.8.100 Port 8080</address>
+</body></html>
